aabc788bed4729fc42429322aea87c59660fde08
Shauna Saunders <shaunacsaunders@outlook.com>
2021-07-06 12:20:07 -0400

    Created template for homepage

--- server.py
+++ server.py
@@ -5,7 +5,12 @@ from flask import Flask, redirect, render_template
 app = Flask(__name__)
 app.secret_key = "dev"
 
+@app.route('/')
+def render_homepage():
+    """Renders the homepage"""
 
-if __name__ = '__main__':
-    connect_to_db(app)
+    return render_template('homepage.html')
+
+
+if __name__ == '__main__':
     app.run(debug=True, use_reloader=True, use_debugger=True)
