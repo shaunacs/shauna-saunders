7eecb4ffdc15ad1d5605849a5d3d4b5679276a37
Shauna Saunders <shaunacsaunders@outlook.com>
2021-07-06 12:13:50 -0400

    Added base html

--- server.py
+++ server.py
@@ -1,3 +1,11 @@
 """Server for Shauna Saunders personal website"""
 
 from flask import Flask, redirect, render_template
+
+app = Flask(__name__)
+app.secret_key = "dev"
+
+
+if __name__ = '__main__':
+    connect_to_db(app)
+    app.run(debug=True, use_reloader=True, use_debugger=True)
