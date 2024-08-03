4caa170812b8e7ba222ef047b53a824fe2ec7cc7
Shauna Saunders <shaunacsaunders@outlook.com>
2024-08-03 10:50:19 -0400

    fix random cat button

--- static/js/random-cat-button.js
+++ static/js/random-cat-button.js
@@ -1,7 +1,17 @@
+let currentIndex = 0;
+
 function getRandomCatPic() {
     const myCatPics = ['Frankie1.jpg', 'Frankie2.jpg', 'Gary-side.PNG', 'Gary.jpg'];
+    
 
-    var catPic = myCatPics[Math.floor(Math.random()*myCatPics.length)];
+    if (myCatPics.length == 0) {
+        console.log('No images available')
+        return null
+    }
+
+    const catPic = myCatPics[currentIndex];
+    currentIndex = (currentIndex + 1) % myCatPics.length;
+    
     return catPic;
 }
 
